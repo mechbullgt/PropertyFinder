9a773ed4080de8ff5977227822606b50eead2915
Maqbool Ansari <maqbool.ansari@mindstix.com>
2018-10-02 01:34:11 +0530

    Separated SearchPage from App.js

--- PropertyFinder/App.js
+++ PropertyFinder/App.js
@@ -1,48 +1,13 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-//String Mode, for better Error handling.
 'use strict';
 
-//1 Importing modules
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import {
+  createStackNavigator,
+} from 'react-navigation';
+import SearchPage from './SearchPage';
 
-// Adding navigation to the application
-import { createStackNavigator } from 'react-navigation';
-
-//3 Define component that represents the UI
-type Props = {};
-
-// Search Page Class.
-class SearchPage extends Component<Props> {
-  static navigationOptions = {
-    title: "Property Finder",
-  };
-  render() {
-    return (
-      <Text style={styles.description}>Search Now for Houses on SALE!!!</Text>
-    );
-  }
-}
-// Search Component Page
 const App = createStackNavigator({
   Home: { screen: SearchPage },
 });
 export default App;
-
-//4 Creates a style object that controls the component’s layout and appearance.
-const styles = StyleSheet.create({
-  description: {
-    fontSize: 18,
-    textAlign: 'center',
-    color: '#656565',
-    marginTop: 65,
-  }
-},
-);
---
+++ PropertyFinder/SearchPage.js
@@ -0,0 +1,47 @@
+'use strict';
+
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+    Button,
+    Image,
+    ActivityIndicator
+} from 'react-native';
+
+type Props = {};
+
+export default class SearchPage extends Component<Props> {
+    static navigationOptions = {
+        title: 'Property Finder',
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.description}>
+                    Search for houses to buy!
+            </Text>
+                <Text style={styles.description}>
+                    Go ahead, search by Place name or Post code.
+            </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    description: {
+        marginBottom: 20,
+        fontSize: 18,
+        textAlign: 'center',
+        color: '#656565'
+    },
+    container :{
+        padding: 30,
+        marginTop: 65,
+        alignItems: 'center'
+    }
+});
